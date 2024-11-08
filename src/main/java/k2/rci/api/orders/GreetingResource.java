c231722a46717e93174d89feecefcddd18a50633
Roger Silva <rogerrafael7@gmail.com>
2024-11-08 04:43:21 -0300

    feat(start): estrutura inicial

---
+++ src/main/java/k2/rci/api/orders/GreetingResource.java
@@ -0,0 +1,16 @@
+package k2.rci.api.orders;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+}
